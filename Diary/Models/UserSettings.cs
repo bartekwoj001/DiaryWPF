c0c4b435e9b886fbf200230e9000810dbee5e1cb
bortek1993 <b.wojciechowski001@gmail.com>
2023-06-13 15:58:38 +0200

    Add project files.

---
+++ Diary/Models/UserSettings.cs
@@ -0,0 +1,170 @@
+﻿using Diary.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diary.Models
+{
+    public class UserSettings : IDataErrorInfo
+    {
+        private bool _isServerAddressValid;
+        private bool _isServerNameValid;
+        private bool _isDatabaseValid;
+        private bool _isUserValid;
+        private bool _isPasswordValid;
+
+
+        public string ServerAddress
+        {
+            get
+            {
+                return Settings.Default.ServerAddress;
+            }
+            set
+            {
+                Settings.Default.ServerAddress = value;
+            }
+        }
+        public string ServerName
+        {
+            get
+            {
+                return Settings.Default.ServerName;
+            }
+            set
+            {
+                Settings.Default.ServerName = value;
+            }
+        }
+        public string Database
+        {
+            get
+            {
+                return Settings.Default.Database;
+            }
+            set
+            {
+                Settings.Default.Database = value;
+            }
+        }
+        public string User
+        {
+            get
+            {
+                return Settings.Default.User;
+            }
+            set
+            {
+                Settings.Default.User = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return Settings.Default.Password;
+            }
+            set
+            {
+                Settings.Default.Password = value;
+            }
+        }
+
+
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case nameof(ServerAddress):
+                        if (string.IsNullOrWhiteSpace(ServerAddress))
+                        {
+                            Error = "Pole jest wymagane.";
+                            _isServerAddressValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isServerAddressValid = true;
+                        }
+                        break;
+
+                    case nameof(ServerName):
+                        if (string.IsNullOrWhiteSpace(ServerAddress))
+                        {
+                            Error = "Pole jest wymagane.";
+                            _isServerNameValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isServerNameValid = true;
+                        }
+                        break;
+
+                    case nameof(Database):
+                        if (string.IsNullOrWhiteSpace(ServerAddress))
+                        {
+                            Error = "Pole jest wymagane.";
+                            _isDatabaseValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isDatabaseValid = true;
+                        }
+                        break;
+
+                    case nameof(User):
+                        if (string.IsNullOrWhiteSpace(ServerAddress))
+                        {
+                            Error = "Pole jest wymagane.";
+                            _isUserValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isUserValid = true;
+                        }
+                        break;
+
+                    case nameof(Password):
+                        if (string.IsNullOrWhiteSpace(ServerAddress))
+                        {
+                            Error = "Pole jest wymagane.";
+                            _isPasswordValid = false;
+                        }
+                        else
+                        {
+                            Error = string.Empty;
+                            _isPasswordValid = true;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                return Error;
+            }
+        }
+        public string Error { get; set; }
+
+        public bool IsValid
+        {
+            get
+            {
+                return _isServerAddressValid && _isServerNameValid && _isPasswordValid && 
+                    _isDatabaseValid && _isUserValid;
+            }
+        }
+
+        public void Save()
+        {
+            Settings.Default.Save();
+        }
+    }
+}
---
+++ Diary/ViewModels/MainViewModel.cs
@@ -0,0 +1,202 @@
+﻿using Diary.Commands;
+using Diary.Models;
+using Diary.Models.Wrappers;
+using Diary.Views;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Diary.Models.Domains;
+using Group = Diary.Models.Domains.Group;
+
+namespace Diary.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private Repository _repository = new Repository();
+        public MainViewModel()
+        {
+
+            AddStudentCommand = new RelayCommand(AddEditStudent);
+            EditStudentCommand = new RelayCommand(AddEditStudent, CanEditDeleteStudent);
+            DeleteStudentCommand = new AsyncRelayCommand(DeleteStudent, CanEditDeleteStudent);
+            RefreshStudentsCommand = new RelayCommand(RefreshStudents);
+            SettingsCommand = new RelayCommand(SettingsDiary);
+            LoadedWindowCommand = new RelayCommand(LoadedWindow);
+
+            LoadedWindow(null);
+        }
+
+        
+
+        public ICommand RefreshStudentsCommand { get; set; }
+        public ICommand AddStudentCommand { get; set; }
+        public ICommand EditStudentCommand { get; set; }
+        public ICommand DeleteStudentCommand { get; set; }
+        public ICommand SettingsCommand { get; set; }
+        public ICommand LoadedWindowCommand { get; set; }
+
+
+
+        private StudentWrapper _selectedStudent;
+         
+        public StudentWrapper SelectedStudent
+        {
+            get { return _selectedStudent; }
+            set 
+            { 
+                _selectedStudent = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<StudentWrapper> _students;
+
+        public ObservableCollection<StudentWrapper> Students
+        {
+            get { return _students; }
+            set
+            {
+                _students = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _selectedGroupId;
+
+        public int SelectedGroupId
+        {
+            get { return _selectedGroupId; }
+            set 
+            { 
+                _selectedGroupId = value;
+                OnPropertyChanged();           
+            }
+        }
+         
+
+        private ObservableCollection<Group> _groups;
+
+        public ObservableCollection<Group> Groups
+        {
+            get { return _groups; }
+            set
+            {
+                _groups = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private async Task DeleteStudent(object obj)
+        {
+            var metroWindow = Application.Current.MainWindow as MetroWindow;
+            var dialog = await metroWindow.ShowMessageAsync("Usuwanie ucznia", $"Czy na pewno chcesz usunąć ucznia {SelectedStudent.FirstName} {SelectedStudent.LastName}?", 
+                MessageDialogStyle.AffirmativeAndNegative);
+
+            if (dialog != MessageDialogResult.Affirmative)
+                return;
+
+            _repository.DeleteStudent(SelectedStudent.Id);
+
+            RefreshDiary();
+        }
+
+        private void AddEditStudent(object obj)
+        {
+            var addEditStudentWindow = new AddEditStudentView(obj as StudentWrapper);
+            addEditStudentWindow.Closed += AddEditStudentWindow_Closed;
+            addEditStudentWindow.ShowDialog();
+        }
+
+        private void AddEditStudentWindow_Closed(object sender, EventArgs e)
+        {
+            RefreshDiary();
+        }
+
+        private void RefreshStudents(object obj)
+        {
+            RefreshDiary();
+        }
+        private bool CanEditDeleteStudent(object obj)
+        {
+            return SelectedStudent != null;
+        }
+
+        private void InitGroups()
+        {
+            var groups = _repository.GetGroups();
+            groups.Insert(0, new Group { Id = 0, Name = "Wszystkie" });
+
+
+            Groups = new ObservableCollection<Group>(groups);
+
+            SelectedGroupId = 0;
+        }
+
+        private void RefreshDiary()
+        {
+            Students = new ObservableCollection<StudentWrapper>(
+                _repository.GetStudents(SelectedGroupId));
+            
+        }
+        private void SettingsDiary(object obj)
+        {
+            var settingsDiaryView = new SettingsDiaryView(true);
+            settingsDiaryView.ShowDialog();
+
+        }
+
+        private bool IsValidConnectionToDatabase()
+        {
+            try
+            {
+                using (var context = new ApplicationDbContext())
+                {
+                    context.Database.Connection.Open();
+                    context.Database.Connection.Close();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        private async void LoadedWindow(object obj)
+        {
+            if (!IsValidConnectionToDatabase())
+            {
+                var metroWindow = Application.Current.MainWindow as MetroWindow;
+                var dialog = await metroWindow.ShowMessageAsync("Błąd połączenia", $"Nie można " +
+                    $"połączyć się z bazą danych. Czy chcesz zmienić ustawienia?",
+                    MessageDialogStyle.Affirmative);
+
+                if (dialog == MessageDialogResult.Negative)
+                {
+                    Application.Current.Shutdown();
+                }
+                else
+                {
+                    var settingsDiaryView = new SettingsDiaryView(true);
+                    settingsDiaryView.ShowDialog();
+                }
+            }
+            else
+            {
+                RefreshDiary();
+                InitGroups();
+            }
+      
+        }
+    }
+}
